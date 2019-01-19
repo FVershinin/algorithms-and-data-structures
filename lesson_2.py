f3d46b54a6081760538a86b7e927064807fd30ae
fvershinin <fedor@vershinin.net>
2019-01-19 02:53:20 +0200

    implemented tasks from lesson 2

--- lesson_2.py
+++ lesson_2.py
@@ -1,3 +1,4 @@
+# 1
 def calculator():
     operation = ""
     while operation != "0":
@@ -21,3 +22,56 @@ def calculator():
         else:
             print("Incorrect operation")
 
+
+# 2
+def count_number_type():
+    text = input("please enter number:")
+    even_numbers = 0
+    odd_numbers = 0
+    for symbol in text:
+        number = int(symbol)
+        if number % 2 == 0:
+            even_numbers += 1
+        else:
+            odd_numbers += 1
+    print(f'odd numbers: {odd_numbers}, even numbers: {even_numbers}')
+
+
+# 3
+def reverse():
+    text = input("Enter text::")
+
+    print(text[::-1])
+
+
+# 4
+def sum():
+    for i in range(0, int(input("Enter count"))):
+        print(1 / pow(2, i) * (1 if i % 2 == 0 else -1))
+
+# 6
+import random
+
+
+def _random():
+    secret_value = random.randint(0, 100)
+    for i in range(10):
+        value = int(input("Please enter number"))
+        if secret_value == value:
+            print('You guessed the number!')
+            break
+        else:
+            print(f'The secret number is {"greater" if secret_value > value else "less"}')
+    print(f'Secret number is ${secret_value}')
+
+# 8
+def calculate_numbers_in_sequence():
+    numbers = input("Enter numbers")
+    assert numbers.isdigit()
+    number = input("Enter number")
+    assert number.isdigit()
+    results = 0
+    for symbol in numbers:
+        if symbol == number:
+            results += 1;
+    print(results)
